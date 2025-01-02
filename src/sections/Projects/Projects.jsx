841f0e7a5832081da5bcb01c43a3198cd8a4774f
guptab8 <guptab8@mcmaster.ca>
2025-01-02 18:02:59 -0500

    Update cover photo of Blast From the Past Elm Game Project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,7 +2,7 @@
 import styles from './ProjectStyles.module.css'
 import ProjectCard from '../../common/ProjectCard'
 
-import viberr from '../../assets/viberr.png'
+import blastfrompast from '../../assets/blast-from-the-past.png'
 import freshBurger from '../../assets/fresh-burger.png'
 import hipsster from '../../assets/hipsster.png'
 import fitLift from '../../assets/fitlift.png'
@@ -15,7 +15,7 @@ function Projects() {
         <h1 className="sectionTitle">Projects</h1>
         <div className={styles.projectsContainer}>
             <ProjectCard 
-                src={viberr} 
+                src={blastfrompast}
                 link="https://www.linkedin.com/in/bhumika-gupta-0552/"
                 h3="Blast From The Past" 
                 p="Parkinson's disease detection Elm game" 
@@ -30,13 +30,13 @@ function Projects() {
                 src={hipsster} 
                 link="https://github.com/bhumika-gupta/gbatteries"
                 h3="GBatteries Battery Tests" 
-                p="Node.js web app project with battery test data" 
+                p="Node.js web app with battery data" 
             />
             <ProjectCard 
                 src={fitLift} 
                 link="https://www.figma.com/proto/VPwazFDDf1OHCLXjXJbdMu/LEAP-Pathfinder-3-Design%3A-FocusBuds?page-id=0%3A1&node-id=96-2555&viewport=2103%2C-386%2C0.13&t=NGULGlUtsey1ZKaU-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=96%3A2555&show-proto-sidebar=1"
                 h3="FocusBuds" 
-                p="Social productivity app Figma prototype" 
+                p="Social productivity Figma app" 
             />
         </div>
     </section>
