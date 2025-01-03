ca1e4fe5d546c54a707b109bc89baddf92b0424e
guptab8 <guptab8@mcmaster.ca>
2025-01-02 19:58:37 -0500

    Update cover photos of Tic-Tac-Toe, GBatteries, and FocusBuds projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -3,9 +3,9 @@ import styles from './ProjectStyles.module.css'
 import ProjectCard from '../../common/ProjectCard'
 
 import blastfrompast from '../../assets/blast-from-the-past.png'
-import freshBurger from '../../assets/fresh-burger.png'
-import hipsster from '../../assets/hipsster.png'
-import fitLift from '../../assets/fitlift.png'
+import tictactoe from '../../assets/tic-tac-toe.png'
+import gbatteries from '../../assets/gbatteries-logo.png'
+import focusbuds from '../../assets/focusbuds.png'
 
 // can further reduce redundancy by mapping over array of project info
 
@@ -21,19 +21,19 @@ function Projects() {
                 p="Parkinson's disease detection Elm game" 
             />
             <ProjectCard 
-                src={freshBurger} 
+                src={tictactoe} 
                 link="https://github.com/bhumika-gupta/tic-tac-toe-react"
                 h3="Tic Tac Toe" 
                 p="React game" 
             />
             <ProjectCard 
-                src={hipsster} 
+                src={gbatteries} 
                 link="https://github.com/bhumika-gupta/gbatteries"
                 h3="GBatteries Battery Tests" 
                 p="Node.js web app with battery data" 
             />
             <ProjectCard 
-                src={fitLift} 
+                src={focusbuds} 
                 link="https://www.figma.com/proto/VPwazFDDf1OHCLXjXJbdMu/LEAP-Pathfinder-3-Design%3A-FocusBuds?page-id=0%3A1&node-id=96-2555&viewport=2103%2C-386%2C0.13&t=NGULGlUtsey1ZKaU-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=96%3A2555&show-proto-sidebar=1"
                 h3="FocusBuds" 
                 p="Social productivity Figma app" 
