eb7e9a00cbf5ef7148d5f7324ef12fca1a93d8ef
guptab8 <guptab8@mcmaster.ca>
2026-08-22 12:55:45 -0400

    update skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -14,27 +14,35 @@ function Skills() {
         <section id="skills" className={styles.container}>
             <h1 className="sectionTitle">Skills</h1>
             <div className={styles.skillList}>
-                <SkillList src={checkMarkIcon} skill="HTML" />
-                <SkillList src={checkMarkIcon} skill="CSS" />
-                <SkillList src={checkMarkIcon} skill="JavaScript" />
-                <SkillList src={checkMarkIcon} skill="Elm" />
+                <SkillList src={checkMarkIcon} skill="Python" />
                 <SkillList src={checkMarkIcon} skill="Java" />
+                <SkillList src={checkMarkIcon} skill="JavaScript" />
+                <SkillList src={checkMarkIcon} skill="SQL" />
                 <SkillList src={checkMarkIcon} skill="C" />
+                <SkillList src={checkMarkIcon} skill="Scala" />
+                <SkillList src={checkMarkIcon} skill="Elm" />
+                <SkillList src={checkMarkIcon} skill="HTML/CSS" />
             </div>
             <hr />
             <div className={styles.skillList}>
                 <SkillList src={checkMarkIcon} skill="React" />
-                <SkillList src={checkMarkIcon} skill="Node.js" />
                 <SkillList src={checkMarkIcon} skill="Next.js" />
+                <SkillList src={checkMarkIcon} skill="Node.js" />
                 <SkillList src={checkMarkIcon} skill="Flask" />
+                <SkillList src={checkMarkIcon} skill="Pandas" />
+                <SkillList src={checkMarkIcon} skill="Matplotlib" />
             </div>
             <hr />
             <div className={styles.skillList}>
                 <SkillList src={checkMarkIcon} skill="Git" />
+                <SkillList src={checkMarkIcon} skill="Databricks" />
+                <SkillList src={checkMarkIcon} skill="SQL Server" />
+                <SkillList src={checkMarkIcon} skill="Toad" />
+                <SkillList src={checkMarkIcon} skill="Azure" />
+                <SkillList src={checkMarkIcon} skill="Db2" />
+                <SkillList src={checkMarkIcon} skill="DBeaver" />
+                <SkillList src={checkMarkIcon} skill="Excel VBA" />
                 <SkillList src={checkMarkIcon} skill="Figma" />
-                <SkillList src={checkMarkIcon} skill="VS Code" />
-                <SkillList src={checkMarkIcon} skill="PyCharm" />
-                <SkillList src={checkMarkIcon} skill="Eclipse" />
             </div>
         </section>
     )
