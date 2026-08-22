8dc41d06249425fdb665b39e4b8bab1492702b6d
guptab8 <guptab8@mcmaster.ca>
2026-08-22 15:13:34 -0400

    update about me description

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -52,7 +52,7 @@ function Hero() {
                         <img src={githubIcon} alt="Github icon" />
                     </a>
                 </span>
-                <p className={styles.description}>A third year undergraduate Computer Science student at McMaster University. Seeking learning experiences and opportunities to aid in both personal and professional development. Especially interested in opportunities in coding and design!</p>
+                <p className={styles.description}>An incoming fourth year undergraduate Computer Science student at McMaster University. Seeking learning experiences and opportunities to aid in both personal and professional development. Especially interested in opportunities in making meaningful impact through software development and tooling!</p>
                 <a href={resume} download>
                     <button className="hover">
                         Resume
